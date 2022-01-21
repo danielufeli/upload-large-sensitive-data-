abc9647b076c084d2423c4c35a524184a5158fd5
danielufeli <danielufeli@yahoo.com>
2022-01-22 00:53:37 +0100

    Add and configure mongodb

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import fileUpload from 'express-fileupload';
 import routes from './routes/index.js';
+import connectDB from '../config/db.js';
 
 const app = express();
 
+connectDB();
+
 app.set('views', './client');
 app.set('view engine', 'ejs');
+app.use(express.static('client/public'));
 
 routes(app);
 
