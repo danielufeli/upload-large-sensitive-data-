a709331fd8d4901bc7b6a2760db4cce4f068c79a
danielufeli <danielufeli@yahoo.com>
2022-01-22 16:26:41 +0100

    Add Style'

--- server/controllers/uploadController.js
+++ server/controllers/uploadController.js
@@ -20,6 +20,38 @@ class uploadController {
       res.status(500).json(error);
     }
   }
+
+  static async getUploads(req, res, next) {
+    try {
+      const { title, description } = req.body;
+      const file = req.files.mFile;
+
+      const fileName =
+        new Date().getTime().toString() + path.extname(file.name);
+      const savePath = path.join(__dirname, 'public', 'uploads', fileName);
+      console.log(savePath);
+      await file.mv(savePath);
+      return res.status(201).json({ status: 'success' });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+
+  static async deleteUpload(req, res, next) {
+    try {
+      const { title, description } = req.body;
+      const file = req.files.mFile;
+
+      const fileName =
+        new Date().getTime().toString() + path.extname(file.name);
+      const savePath = path.join(__dirname, 'public', 'uploads', fileName);
+      console.log(savePath);
+      await file.mv(savePath);
+      return res.status(201).json({ status: 'success' });
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
 }
 
 export default uploadController;
