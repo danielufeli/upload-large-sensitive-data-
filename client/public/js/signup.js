9fa2a29e8e7cd423ad77bf94b6c3f427a8028583
danielufeli <danielufeli@yahoo.com>
2022-01-24 08:42:31 +0100

    Fix issue with signup and signin

--- client/public/js/signup.js
+++ client/public/js/signup.js
@@ -1,12 +1,15 @@
-const signForm = () => {
+const signUpForm = (e) => {
+  e.preventDefault();
   const userEmail = document.getElementById('email').value;
   const userPassword = document.getElementById('password').value;
+  const userName = document.getElementById('name').value;
 
-  const url = '/api/v1/auth/signin';
+  const url = '/api/v1/auth/signup';
 
-  const signInData = {
+  const signUpData = {
     email: userEmail,
     password: userPassword,
+    name: userName,
   };
   document.getElementById('errorlog').innerHTML =
     '<img src="./public/img/loading.gif">';
@@ -15,7 +18,7 @@ const signForm = () => {
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(signInData),
+    body: JSON.stringify(signUpData),
   })
     .then((res) => res.json())
     .then((body) => {
@@ -25,11 +28,19 @@ const signForm = () => {
       } else {
         const msg = body.errors.map((m) => {
           if (m.msg) return m.msg;
-          if (m.email) return m.email;
+          if (m.email) return '<br>' + m.email + ' Please Check your email';
+          if (m.password)
+            return (
+              '<br>' +
+              m.password +
+              ' Please Check your Password it must be 6 letters'
+            );
+          if (m.name) return '<br>' + m.name + ' Please Check your Name';
         });
+        console.log(body.errors.map((m) => m.msg));
         document.getElementById('errorlog').innerHTML = msg;
       }
     });
 };
-const signBtn = document.getElementById('loginBtn');
-signBtn.addEventListener('click', signForm);
+const signupBtn = document.getElementById('signupBtn');
+signupBtn.addEventListener('click', signUpForm);
